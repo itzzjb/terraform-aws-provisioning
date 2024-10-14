67e80b63ed07b6a741e5078c9322e101feeaa2fe
Januda Bethmin <desilvabethmin@gmail.com>
2024-10-14 13:20:26 +0530

    creating a public subnet

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# creating the resources here
+# creating all the resources here
 
 # creating a vpc
 resource "aws_vpc" "my_vpc" {
@@ -12,4 +12,21 @@ resource "aws_vpc" "my_vpc" {
         # by adding the Name tag, it will show the value in the UI of the aws console for the VPC
         Name = "dev"
     }
+}
+
+# creating a subnet (public)
+resource "aws_subnet" "my_public_subnet" {
+  # need to specify the vpc that we are going to use for the public subnet (aws_vpc.my_vpc)
+  # getting the the id using the output reference variable
+  vpc_id = aws_vpc.my_vpc.id
+  # configure the cidr_block of the subnet inside the cidr_block of the vpc
+  cidr_block = "10.0.1.0/24"
+  # this will make this subnet a public subnet
+  map_public_ip_on_launch = true
+  # we need to specify an availability zone for a subnet
+  availability_zone = "us-east-1a"
+  # adding a tag
+  tags = {
+    Name = "dev-public"
+  }
 }
